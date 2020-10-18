7690a0b3c87d06d655fd72eae0b6b8e35c0d4b35
Michelle-Lefebvre <mmldesigns007@gmail.com>
2020-10-18 10:18:38 -0400

    header initial comit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCZoNKtoJ13piLRNkukPamGO566d3iJ8Po",
+    authDomain: "ia-131da.firebaseapp.com",
+    databaseURL: "https://ia-131da.firebaseio.com",
+    projectId: "ia-131da",
+    storageBucket: "ia-131da.appspot.com",
+    messagingSenderId: "618723001556",
+    appId: "1:618723001556:web:2cb22b7d8848c9825f6741",
+    measurementId: "G-6FRTN35XM7"
+};
